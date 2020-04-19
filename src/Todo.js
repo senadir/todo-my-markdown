7a7c48f5b06333cf0178a2554216006730b2ea27
Seghir Nadir <nadir.seghir@gmail.com>
2020-04-20 00:34:58 +0100

    add markdown render support in todo lists

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-
+import marked from 'marked';
 export default function Todo( { id, todo, done, todoChildren, updateTodo } ) {
+	const renderer = new marked.Renderer();
+	renderer.paragraph = ( text ) => text;
 	return (
 		<li className="todo-item">
 			<input
@@ -9,7 +12,12 @@ export default function Todo( { id, todo, done, todoChildren, updateTodo } ) {
 				checked={ done ? 'checked' : '' }
 				onChange={ () => updateTodo( id, ! done ) }
 			/>
-			<label htmlFor={ id }>{ todo }</label>
+			<label
+				htmlFor={ id }
+				dangerouslySetInnerHTML={ {
+					__html: marked( todo, { renderer } ),
+				} }
+			></label>
 			{ todoChildren && (
 				<ul className="todo-list">
 					{ todoChildren.map( ( childTodo ) => (
