d439cdf53eec9ee2ef6a7aa2f4cf11dbf0af616e
Seghir Nadir <nadir.seghir@gmail.com>
2020-05-05 00:51:05 +0100

    focus on label

--- packages/components/src/checkbox.js
+++ packages/components/src/checkbox.js
@@ -7,7 +7,6 @@ export default function Checkbox( { id, checked, onChange, label, children } ) {
 		<div
 			sx={ {
 				mb: 'xsmall',
-				pl: 'medium',
 				position: 'relative',
 			} }
 		>
@@ -21,14 +20,15 @@ export default function Checkbox( { id, checked, onChange, label, children } ) {
 			/>
 			<label
 				htmlFor={ id }
+				tabIndex={ 0 }
 				sx={ {
 					position: 'relative',
 					cursor: 'pointer',
+					pl: 'medium',
 					textDecoration: 'line-through',
 					transition: 'all ease 300ms',
 					'&:focus': {
-						outline: ( theme ) =>
-							`1px solid ${ theme.colors.text }`,
+						outline: 'none',
 					},
 					textDecorationColor: checked ? 'text' : 'transparent',
 					code: {
@@ -39,46 +39,45 @@ export default function Checkbox( { id, checked, onChange, label, children } ) {
 				{ createElement( 'span', {
 					dangerouslySetInnerHTML: { __html: label },
 				} ) }
+				<div
+					aria-hidden="true"
+					sx={ {
+						display: 'inline-block',
+						verticalAlign: 'text-top',
+						width: '18px',
+						height: '18px',
+						bg: checked ? 'primary' : 'transparent',
+						top: '3px',
+						left: 0,
+						position: 'absolute',
+						border: '1px solid',
+						borderColor: 'primary',
+						transition: 'all ease 300ms',
+						outline: '1px solid transparent',
+						'label:hover &, label:focus &': {
+							boxShadow: ( theme ) =>
+								! checked &&
+								`0 0 3px 0px ${ theme.colors.primary }`,
+						},
+					} }
+				></div>
+				<svg
+					aria-hidden="true"
+					sx={ {
+						position: 'absolute',
+						left: '1px',
+						top: '3px',
+						fill: 'background',
+					} }
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					width="16px"
+					height="16px"
+				>
+					<path d="M0 0h24v24H0z" fill="none" />
+					<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+				</svg>
 			</label>
-			<div
-				aria-hidden="true"
-				tabIndex={ 0 }
-				sx={ {
-					display: 'inline-block',
-					verticalAlign: 'text-top',
-					width: '18px',
-					height: '18px',
-					bg: checked ? 'primary' : 'transparent',
-					top: '3px',
-					left: 0,
-					position: 'absolute',
-					border: '1px solid',
-					borderColor: 'primary',
-					transition: 'all ease 300ms',
-					outline: '1px solid transparent',
-					'label:hover + &, &:focus': {
-						boxShadow: ( theme ) =>
-							! checked &&
-							`0 0 3px 0px ${ theme.colors.primary }`,
-					},
-				} }
-			></div>
-			<svg
-				aria-hidden="true"
-				sx={ {
-					position: 'absolute',
-					left: '1px',
-					top: '3px',
-					fill: 'background',
-				} }
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				width="16px"
-				height="16px"
-			>
-				<path d="M0 0h24v24H0z" fill="none" />
-				<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
-			</svg>
 			{ children }
 		</div>
 	);
